5695c4eba644a5315ca1903e9b2eb559253c019a
sokal <sokalski.jakub98@gmail.com>
2021-12-12 14:04:46 +0100

    Dictionary comprehension added

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 result_dict = {word: len(word) for word in sentence.split()}
 print(result_dict)
 
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {key: (temp_c * 9/5) + 32 for key, temp_c in weather_c.items()}
+print(weather_f)
+
 
 
 
