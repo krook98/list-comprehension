563554650a4f6c524599acab9b3f8969fdbb7f1c
sokal <sokalski.jakub98@gmail.com>
2021-12-12 13:24:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+with open('file1.txt') as f1:
+    lines_1 = [line[:-1] for line in f1]
+    print(lines_1)
+
+with open('file2.txt') as f2:
+    lines_2 = [line[:-1] for line in f2]
+    print(lines_2)
+
+result = [number for number in (lines_1 and lines_2)
+          if number in lines_1 and number in lines_2]
+
+print(result)
